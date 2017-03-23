1d16fa3ed03b42eacbf112346001d0e67fb3f0b8
Amit Manchanda <amitdelhi1995@gmail.com>
2017-03-23 19:13:40 +0530

    Changed architecture

--- src/nn_training.py
+++ src/nn_training.py
@@ -20,35 +20,7 @@ from extract_patches import get_data_training
 #Define the neural network
 def get_unet(n_ch,patch_height,patch_width):
     inputs = Input((n_ch, patch_height, patch_width))
-    #conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
-    #conv1 = Dropout(0.2)(conv1)
-    #pool1 = MaxPooling2D(pool_size=(2,2))(conv1)
-    #conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
-    #pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    #
-    #conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool1)
-    #conv2 = Dropout(0.2)(conv2)
-    #conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv2)
-    #pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    #
-    #conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool2)
-    #conv3 = Dropout(0.2)(conv3)
-    #conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv3)
-
-    #up1 = merge([UpSampling2D(size=(2, 2))(conv3), conv2], mode='concat', concat_axis=1)
-    #conv4 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool1)
-    #conv4 = Dropout(0.2)(conv4)
-    #conv4 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv4)
-    #
-    #up2 = merge([UpSampling2D(size=(2, 2))(conv4), conv1], mode='concat', concat_axis=1)
-    #conv5 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv4)
-    #conv5 = Dropout(0.2)(conv5)
-    #conv5 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv5)
-    #
-    #conv6 = Convolution2D(2, 1, 1, activation='relu',border_mode='same')(conv5)
-    #conv6 = core.Reshape((2,patch_height*patch_width))(conv6)
-    #conv6 = core.Permute((2,1))(conv6)
-    ############
+    
     dnn = Flatten()(inputs)
     dnn1 = Dense(256)(dnn) 
     conv6 = Dense(2)(dnn1)
@@ -98,11 +70,11 @@ model = get_unet(n_ch, patch_height, patch_width)  #the U-net model
 print "Check: final output of the network:"
 print model.output_shape
 model.summary()
-plot(model, to_file='./'+name_experiment+'/'+name_experiment + '_model.png')   #check how the model looks like
+plot(model, to_file='./'+name_experiment+'/nn'+'/'+name_experiment + '_model.png')   #check how the model looks like
 json_string = model.to_json()
-open('./'+name_experiment+'/'+name_experiment +'_architecture.json', 'w').write(json_string)
+open('./'+name_experiment+'/nn'+'/'+name_experiment +'_architecture.json', 'w').write(json_string)
 
-checkpointer = ModelCheckpoint(filepath='./'+name_experiment+'/'+name_experiment +'_best_weights.h5', verbose=1, monitor='val_loss', mode='auto', save_best_only=True) #save at each epoch if the validation decreased
+checkpointer = ModelCheckpoint(filepath='./'+name_experiment+'/nn'+'/'+name_experiment +'_best_weights.h5', verbose=1, monitor='val_loss', mode='auto', save_best_only=True) #save at each epoch if the validation decreased
 
 
 patches_masks_train = np_utils.to_categorical(patches_masks_train, 2)
@@ -112,4 +84,4 @@ for i in range(N_epochs):
     fa, fr, ta, tr = class_accuracy(y_pred[:, 1], patches_masks_train[:, 1])
     print "FA FR TA TR", fa, fr, ta, tr
 
-model.save_weights('./'+name_experiment+'/'+name_experiment +'_last_weights.h5', overwrite=True)
+model.save_weights('./'+name_experiment+'/nn/'+'/'+name_experiment +'_last_weights.h5', overwrite=True)
