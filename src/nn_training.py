58b191ed112dbf768897a222e5049cfe397b5280
Amit Manchanda <amitdelhi1995@gmail.com>
2017-03-24 01:44:03 +0530

    Bug fixed

--- src/nn_training.py
+++ src/nn_training.py
@@ -109,4 +109,4 @@ model.save_weights('./'+name_experiment+'/'+algorithm+'/'+name_experiment +'_las
 y_pred = model.predict(patches_imgs_val, batch_size=32, verbose=1)
 print roc_auc_score(patches_mask_val[:,1], y_pred[:,1])
 skplt.plot_roc_curve(patches_masks_val[:,1], y_pred[:,1])
-plt.show()
+plt.savefig('./'+name_experiment+'/'+algorithm+'/'+name_experiment +'_roc.png')
