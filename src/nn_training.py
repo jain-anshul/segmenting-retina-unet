0624e368396ff078f5afe7cab665c8fde0c22e1d
Amit Manchanda <amitdelhi1995@gmail.com>
2017-03-24 02:28:49 +0000

    ROC nn

--- src/nn_training.py
+++ src/nn_training.py
@@ -110,5 +110,5 @@ y_pred = model.predict(patches_imgs_val, batch_size=32, verbose=1)
 
 print '\n', roc_auc_score(patches_masks_val[:,1], y_pred[:,1])
 print y_pred[:,1].shape, patches_masks_val[:,1].shape
-skplt.plot_roc_curve(patches_masks_val[:,1], y_pred)
+skplt.plot_roc_curve(patches_masks_val[:,1], y_pred, curves=('each_class'))
 plt.savefig('./'+name_experiment+'/'+algorithm+'/'+name_experiment +'_roc.png')
