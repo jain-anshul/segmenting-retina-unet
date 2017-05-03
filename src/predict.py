3829bb8938efadf4cd47ca12d3a60071928f5c3d
Amit Manchanda <amitdelhi1995@gmail.com>
2017-05-03 11:51:50 +0100

    EYE image

--- src/predict.py
+++ src/predict.py
@@ -84,4 +84,4 @@ for index in range(1):
 
 	# ===== Convert the prediction arrays in corresponding images
 
-	pred_img = conv_to_imgs(pred=predictions,img_h=img_truth.shape[2],img_w=img_truth.shape[3],mode='original', patch_h=patch_height, patch_w=patch_width, path_experiment = path_experiment, index=index)
+	pred_img = conv_to_imgs(pred=predictions,img_h=img_truth.shape[2],img_w=img_truth.shape[3],mode='threshold', patch_h=patch_height, patch_w=patch_width, path_experiment = path_experiment, index=index)
